3a8ae29fdfce937e5b69ccaad3604fa43ef33199
emileyski <96009930+emileyski@users.noreply.github.com>
2023-12-24 00:46:41 +0200

    #

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,13 +15,6 @@ const Projects = () => {
       }}
     >
       <ProjectItem
-        title={"Ticketing"}
-        description={
-          "A small microservice application that contained logic for ordering tickets for events. It was built with Node.js, React, Docker, Kubernetes, and MongoDB."
-        }
-        githubLink={"https://github.com/emileyski/ticketing"}
-      />
-      <ProjectItem
         title={"Skillhub"}
         description={
           "“SkillHub” is the online – system for creative people, who want to share their creativity and learn something new about art. It was built with React, Redux, Nest.js, Docker, and PostgreSQL."
@@ -32,6 +25,13 @@ const Projects = () => {
         title={"Pharmacy"}
         description={`The Nest.js backend API, utilizing MS SQL, incorporates JWT for secure routes and role-based access. Swagger ensures precise REST API documentation. It generates PDF receipts, provides basic statistics on sales, and medicine popularity.`}
         githubLink={" https://github.com/emileyski/apteka-api"}
+      />
+      <ProjectItem
+        title={"Ticketing"}
+        description={
+          "A small microservice application that contained logic for ordering tickets for events. It was built with Node.js, React, Docker, Kubernetes, and MongoDB."
+        }
+        githubLink={"https://github.com/emileyski/ticketing"}
       />
     </Container>
   );
