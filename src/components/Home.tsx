6cac8b241b412cef8018e56d8933992836531dcc
emileyski <96009930+emileyski@users.noreply.github.com>
2023-12-26 16:11:25 +0200

    feat: added lazy loading for image at "Home"

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,11 @@
 import { useTranslation } from "react-i18next";
 import styles from "./Home.module.css";
 import { Container } from "@mui/material";
+// import { css } from "@emotion/react";
+import { GridLoader } from "react-spinners"; // Import the ClipLoader component from react-spinners
+import React from "react";
+
+const LazyLoadedImage = React.lazy(() => import("./LazyLoadedImage"));
 
 const Home = () => {
   const { t } = useTranslation();
@@ -18,11 +23,16 @@ const Home = () => {
     >
       <div className={styles["home__text"]}>{t("home.title")} </div>
       <div className={styles.image}>
-        <img className={styles.maskGroupIcon} alt="" src="my-img.png" />
-        <div
-          className={styles.aGraphicDesigner}
-          //TODO: add a link to my CV
+        <React.Suspense
+          fallback={
+            <div className={styles.spinnerContainer}>
+              <GridLoader color={"#b8ff20"} size={50} loading={true} />
+            </div>
+          }
         >
+          <LazyLoadedImage />
+        </React.Suspense>
+        <div className={styles.aGraphicDesigner}>
           {t("home.subtitle-1")} <br />
           {t("home.subtitle-2")}
         </div>
---
+++ src/components/LazyLoadedImage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+const LazyLoadedImage = () => {
+  return <img className={styles.maskGroupIcon} alt="" src="my-img.png" />;
+};
+
+export default LazyLoadedImage;
